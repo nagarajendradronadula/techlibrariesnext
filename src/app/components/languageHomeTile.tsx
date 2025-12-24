5df2d1b26b35b146767a0cdfc596d42b91a5312e
NAGA RAJENDRA DRONADULA <127020330+nagarajendradronadula@users.noreply.github.com>
2025-12-24 08:08:58 +0530

    home page card update

--- src/app/components/languageHomeTile.tsx
+++ src/app/components/languageHomeTile.tsx
@@ -27,17 +27,18 @@ export default function LanguageHomeTile({
           isHovered
             ? "hover:shadow-2xl hover:scale-110 duration-150 transition-all ease-in-out"
             : ""
-        } cursor-pointer rounded-2xl ${className}`}
+        } cursor-pointer rounded-2xl`}
         href="/LanguagePage/[languageId]"
         as={`/LanguagePage/${languageId}`}
-        style={{ transformStyle: 'preserve-3d', width: `${width * 5}px`, height: `${height * 5}px` }}
+        style={{ transformStyle: 'preserve-3d' }}
+        className={`w-32 h-32 sm:w-36 sm:h-36 md:w-40 md:h-40 lg:w-44 lg:h-44 xl:w-48 xl:h-48 ${className}`}
       >
         <img
           src={languageLogo}
           alt={languageName}
-          className="w-32 h-32 m-5"
+          className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 xl:w-28 xl:h-28 m-2 sm:m-3 md:m-4 lg:m-5"
         ></img>
-        <p className="text-4xl font-semibold m-5">{languageName}</p>
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl font-semibold m-1 sm:m-2 md:m-3 lg:m-4">{languageName}</p>
       </Link>
     </div>
   );
