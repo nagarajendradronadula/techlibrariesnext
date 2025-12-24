a08522550e6d6250684136ea53e32d88e00d5b20
NAGA RAJENDRA DRONADULA <127020330+nagarajendradronadula@users.noreply.github.com>
2025-12-24 08:16:35 +0530

    home page card update

--- src/app/components/languageHomeTile.tsx
+++ src/app/components/languageHomeTile.tsx
@@ -27,18 +27,17 @@ export default function LanguageHomeTile({
           isHovered
             ? "hover:shadow-2xl hover:scale-110 duration-150 transition-all ease-in-out"
             : ""
-        } cursor-pointer rounded-2xl`}
+        } cursor-pointer rounded-2xl w-32 h-32 sm:w-36 sm:h-36 md:w-40 md:h-40 lg:w-44 lg:h-44 xl:w-48 xl:h-48 ${className}`}
         href="/LanguagePage/[languageId]"
         as={`/LanguagePage/${languageId}`}
         style={{ transformStyle: 'preserve-3d' }}
-        className={`w-32 h-32 sm:w-36 sm:h-36 md:w-40 md:h-40 lg:w-44 lg:h-44 xl:w-48 xl:h-48 ${className}`}
       >
         <img
           src={languageLogo}
           alt={languageName}
           className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 xl:w-28 xl:h-28 m-2 sm:m-3 md:m-4 lg:m-5"
         ></img>
-        <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl font-semibold m-1 sm:m-2 md:m-3 lg:m-4">{languageName}</p>
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl font-semibold m-1 sm:m-2 md:m-3 lg:m-4 text-white">{languageName}</p>
       </Link>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
             />
           </div>
         
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 sm:gap-8 md:gap-10 max-w-[90rem] mx-auto place-items-center">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 md:gap-8 lg:gap-10 max-w-[90rem] mx-auto place-items-center">
             {languageData.map((language) => (
               <LanguageHomeTile
                 languageId={language.id}
